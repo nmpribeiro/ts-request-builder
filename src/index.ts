7d3bcbec6648e8ff71948a1adf6a63392ee80498
Nuno Ribeiro <nmpribeiro@gmail.com>
2021-12-09 17:00:32 +0100

    Adds HeadersBuilder to the lib

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { fetchWithTimeout } from './FetchWithTimeout';
+import { HeadersBuilder } from './HeadersBuilder';
 import { HTTPMethod, RequestBuilder } from './RequestBuilder';
 
-export { fetchWithTimeout, RequestBuilder, HTTPMethod };
+export { fetchWithTimeout, RequestBuilder, HTTPMethod, HeadersBuilder };
